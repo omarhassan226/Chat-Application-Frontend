8572039db084db308413fe81bf375b916996e0d8
omarhassan226 <oh.22697@gmail.com>
2025-08-14 00:36:23 +0300

    deploying

--- src/app/features/chat/chat-layout/chat-layout.component.ts
+++ src/app/features/chat/chat-layout/chat-layout.component.ts
@@ -1,9 +1,34 @@
-import { AfterViewChecked, Component, ElementRef, NgZone, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  NgZone,
+  QueryList,
+  Renderer2,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { AuthService } from '../../../core/services/auth/auth.service';
 import { ChatService } from '../../../core/services/chat/chat.service';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { SocketService } from '../../../core/services/socket/socket.service';
-import { catchError, debounceTime, distinctUntilChanged, filter, finalize, of, Subject, switchMap, takeUntil, tap } from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  finalize,
+  of,
+  Subject,
+  switchMap,
+  takeUntil,
+  tap,
+} from 'rxjs';
 import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
@@ -17,14 +42,11 @@ import { animate, style, transition, trigger } from '@angular/animations';
         style({ opacity: 0 }),
         animate('300ms ease-in', style({ opacity: 1 })),
       ]),
-      transition(':leave', [
-        animate('300ms ease-out', style({ opacity: 0 })),
-      ]),
+      transition(':leave', [animate('300ms ease-out', style({ opacity: 0 }))]),
     ]),
   ],
 })
 export class ChatLayoutComponent implements AfterViewChecked {
-
   @ViewChild('messageContainer') msgContainer!: ElementRef;
   @ViewChildren('msgElem') messageElems!: QueryList<ElementRef>;
 
@@ -35,12 +57,12 @@ export class ChatLayoutComponent implements AfterViewChecked {
   user1Data: any;
   user2Data: any;
   textMessage: any;
-  chatForm: any = FormGroup
+  chatForm: any = FormGroup;
   isSearching = false;
   searchControl = new FormControl('');
-  isTyping!: any
-  showPicker: any = false
-  selectedFile!: any
+  isTyping!: any;
+  showPicker: any = false;
+  selectedFile!: any;
   currentRoomId!: string;
   typingUsers = new Set<string>();
   showSidebarContent: boolean = false;
@@ -54,10 +76,17 @@ export class ChatLayoutComponent implements AfterViewChecked {
   private destroy$ = new Subject<void>();
   starredUserIds = new Set<string>();
   private messageObserver!: IntersectionObserver;
-  constructor(private renderer: Renderer2, private authService: AuthService, private chatService: ChatService, private fb: FormBuilder, private socket: SocketService, private ngZone: NgZone) {
+  constructor(
+    private renderer: Renderer2,
+    private authService: AuthService,
+    private chatService: ChatService,
+    private fb: FormBuilder,
+    private socket: SocketService,
+    private ngZone: NgZone
+  ) {
     this.chatForm = this.fb.group({
-      text: ['', Validators.required]
-    })
+      text: ['', Validators.required],
+    });
   }
 
   ngAfterViewChecked() {
@@ -68,89 +97,95 @@ export class ChatLayoutComponent implements AfterViewChecked {
     try {
       const el = this.msgContainer.nativeElement;
       el.scrollTop = el.scrollHeight;
-    } catch { }
+    } catch {}
   }
 
   ngAfterViewInit() {
     this.messageElems.changes.subscribe((qlist: QueryList<ElementRef>) => {
-      qlist.forEach(el => this.messageObserver.observe(el.nativeElement));
+      qlist.forEach((el) => this.messageObserver.observe(el.nativeElement));
     });
   }
 
   ngOnInit() {
-    this.theme = localStorage.getItem('theme') as any || 'light';
+    this.theme = (localStorage.getItem('theme') as any) || 'light';
     this.applyTheme();
     this.getMe();
     this.getAllUsers();
     this.getRecentUsersList();
     this.getRecentGroupUsers();
     this.getStaredUsers();
-    this.loadConversations()
+    this.loadConversations();
     this.getStaredUsers();
-    this.socket.listen<any>('receivePrivateMessage')
-      .subscribe(msg => {
-        this.privateMessages.push(msg);
-        console.log('Received private message:', msg);
-      });
+    this.socket.listen<any>('receivePrivateMessage').subscribe((msg) => {
+      this.privateMessages.push(msg);
 
-    this.searchControl.valueChanges.pipe(
-      debounceTime(300),
-      filter((term): term is string => typeof term === 'string'),
-      distinctUntilChanged(),
-      tap(() => this.isSearching = true),
-      switchMap(term => {
-        if (term.length > 0) {
-          return this.chatService.searchUsers(term).pipe(
-            catchError(() => of([])),
-            finalize(() => this.isSearching = false)
-          );
-        } else {
-          return this.chatService.getAllUsers().pipe(
-            catchError(() => of([])),
-            finalize(() => this.isSearching = false)
-          );
-        }
-      }),
-      takeUntil(this.destroy$)
-    ).subscribe(users => {
-      this.users = users;
+      console.log('Received private message:', msg);
     });
 
-    this.socket.listen<{ from: string }>('typing')
-      .subscribe(({ from }) => {
-        if (from === this.user2Data._id) {
-          this.ngZone.run(() => this.isTyping = true);
-          console.log(this.isTyping);
-          this.typingUsers.add(from);
-        }
-      });
-
-    this.socket.listen<{ from: string }>('stopTyping')
-      .subscribe(({ from }) => {
-        if (from === this.user2Data._id) {
-          this.ngZone.run(() => this.isTyping = false);
-          console.log(this.isTyping);
-          this.typingUsers.delete(from);
-        }
-      });
-
-    this.messageObserver = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          const msgId = entry.target.getAttribute('data-msg-id');
-          const msg = this.privateMessages.find((m: any) => m._id === msgId);
-          if (msg && !msg.isRead && msg.senderId === this.user2Data._id) {
-            this.socket.emit('messageRead', {
-              messageId: msg._id,
-              readerId: this.user1Data._id
-            });
+    this.searchControl.valueChanges
+      .pipe(
+        debounceTime(300),
+        filter((term): term is string => typeof term === 'string'),
+        distinctUntilChanged(),
+        tap(() => (this.isSearching = true)),
+        switchMap((term) => {
+          if (term.length > 0) {
+            return this.chatService.searchUsers(term).pipe(
+              catchError(() => of([])),
+              finalize(() => (this.isSearching = false))
+            );
+          } else {
+            return this.chatService.getAllUsers().pipe(
+              catchError(() => of([])),
+              finalize(() => (this.isSearching = false))
+            );
           }
-          this.messageObserver.unobserve(entry.target);
-        }
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((users) => {
+        this.users = users;
       });
-    }, { threshold: 0.5 });
 
-    this.socket.listen<any>('messageSeen')
+    this.socket.listen<{ from: string }>('typing').subscribe(({ from }) => {
+      console.log(this.user2Data);
+      if (from === this.user2Data._id) {
+        this.ngZone.run(() => (this.isTyping = true));
+        console.log(this.isTyping);
+        this.typingUsers.add(from);
+      }
+    });
+
+    this.socket.listen<{ from: string }>('stopTyping').subscribe(({ from }) => {
+      console.log(this.user2Data);
+      if (from === this.user2Data._id) {
+        this.ngZone.run(() => (this.isTyping = false));
+        console.log(this.isTyping);
+        this.typingUsers.delete(from);
+      }
+    });
+
+    this.messageObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const msgId = entry.target.getAttribute('data-msg-id');
+            const msg = this.privateMessages.find((m: any) => m._id === msgId);
+            if (msg && !msg.isRead && msg.senderId === this.user2Data._id) {
+              this.socket.emit('messageRead', {
+                messageId: msg._id,
+                readerId: this.user1Data._id,
+              });
+            }
+            this.messageObserver.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    this.socket
+      .listen<any>('messageSeen')
       .subscribe(({ messageId, readAt }) => {
         const msg = this.privateMessages.find((m: any) => m._id === messageId);
         if (msg) {
@@ -159,34 +194,44 @@ export class ChatLayoutComponent implements AfterViewChecked {
         }
       });
 
-    this.socket.listen<any>('receiveMessage').subscribe(msg => {
-      if (msg.roomId === this.currentRoomId) {
-        this.privateMessages.push(msg);
-      }
+    this.socket.listen<any>('receiveMessage').subscribe((msg) => {
+      console.log(msg);
+      this.getAllUsers();
+      this.getAllUnreadMessages();
+      console.log('unreadedcount', this.getAllUnreadMessages());
+
+      // if (msg.roomId === this.currentRoomId) {
+      this.privateMessages.push(msg);
+      console.log(msg);
+      // }
     });
   }
 
   toggleTheme() {
-    this.theme = (this.theme === 'light') ? 'dark' : 'light';
+    this.theme = this.theme === 'light' ? 'dark' : 'light';
     localStorage.setItem('theme', this.theme);
     this.applyTheme();
   }
 
   applyTheme() {
-    this.renderer.setAttribute(document.documentElement, 'data-bs-theme', this.theme);
+    this.renderer.setAttribute(
+      document.documentElement,
+      'data-bs-theme',
+      this.theme
+    );
   }
 
   onLogout() {
-    this.authService.logout()
+    this.authService.logout();
   }
 
   getMe() {
     this.chatService.getMe().subscribe({
       next: (res: any) => {
-        this.user1Data = res.user
+        this.user1Data = res.user;
         console.log('user1Data', this.user1Data);
-      }
-    })
+      },
+    });
   }
 
   getAllUsers() {
@@ -197,7 +242,7 @@ export class ChatLayoutComponent implements AfterViewChecked {
       },
       error: (err) => {
         console.error('Error fetching users', err);
-      }
+      },
     });
   }
 
@@ -209,22 +254,24 @@ export class ChatLayoutComponent implements AfterViewChecked {
       },
       error: (err: any) => {
         console.error('Error fetching recent group users', err);
-      }
+      },
     });
   }
 
   getPrivateMessages() {
     console.log(this.user2Data._id);
-    this.chatService.getPrivateMessages(this.user1Data._id, this.user2Data._id).subscribe({
-      next: (res: any) => {
-        this.privateMessages = res
-        console.log('Private Messages:', this.privateMessages);
-        this.socket.emit('joinRoom', { roomId: this.currentRoomId });
-      },
-      error: (err: any) => {
-        console.log(err);
-      }
-    })
+    this.chatService
+      .getPrivateMessages(this.user1Data._id, this.user2Data._id)
+      .subscribe({
+        next: (res: any) => {
+          this.privateMessages = res;
+          console.log('Private Messages:', this.privateMessages);
+          this.socket.emit('joinRoom', { roomId: this.currentRoomId });
+        },
+        error: (err: any) => {
+          console.log(err);
+        },
+      });
   }
 
   toggleStar(userId: string): void {
@@ -237,7 +284,7 @@ export class ChatLayoutComponent implements AfterViewChecked {
         },
         error: (err) => {
           console.error('Failed to unstar user', err);
-        }
+        },
       });
     } else {
       this.chatService.starUser(userId).subscribe({
@@ -248,7 +295,7 @@ export class ChatLayoutComponent implements AfterViewChecked {
         },
         error: (err) => {
           console.error('Failed to star user', err);
-        }
+        },
       });
     }
   }
@@ -261,13 +308,15 @@ export class ChatLayoutComponent implements AfterViewChecked {
       },
       error: (err: any) => {
         console.error('Error fetching starred users', err);
-      }
+      },
     });
   }
 
   getUserImage(senderId: string): string {
     const user = this.users.find((u: any) => u._id === senderId);
-    return user.image !== null ? user.image : 'public/images/chat-background.jpg';
+    return user.image !== null
+      ? user.image
+      : 'public/images/chat-background.jpg';
   }
 
   extractFileNameWithoutNumbers(fileUrl: string): string {
@@ -277,7 +326,14 @@ export class ChatLayoutComponent implements AfterViewChecked {
       const fileName = url.pathname.split('/').pop() || '';
       return fileName.replace(/\d+/g, '').replace(/[-_]+/g, ' ').trim();
     } catch {
-      return fileUrl.split('/').pop()?.replace(/\d+/g, '').replace(/[-_]+/g, ' ').trim() || '';
+      return (
+        fileUrl
+          .split('/')
+          .pop()
+          ?.replace(/\d+/g, '')
+          .replace(/[-_]+/g, ' ')
+          .trim() || ''
+      );
     }
   }
 
@@ -291,13 +347,13 @@ export class ChatLayoutComponent implements AfterViewChecked {
     console.log('EMIT typing ➤ to:', this.user2Data._id);
     this.socket.emit('typing', {
       to: this.user2Data._id,
-      userId: this.user1Data._id
+      userId: this.user1Data._id,
     });
     this.typingTimeout = setTimeout(() => {
       console.log('EMIT stopTyping');
       this.socket.emit('stopTyping', {
         to: this.user2Data._id,
-        userId: this.user1Data._id
+        userId: this.user1Data._id,
       });
     }, 2000);
   }
@@ -319,7 +375,7 @@ export class ChatLayoutComponent implements AfterViewChecked {
   }
 
   showEmoji() {
-    this.showPicker = !this.showPicker
+    this.showPicker = !this.showPicker;
     console.log(this.showEmoji);
   }
 
@@ -327,16 +383,16 @@ export class ChatLayoutComponent implements AfterViewChecked {
     return {
       suggestionEmojis: {
         storage: 'localstorage',
-        allowAutoSave: true
+        allowAutoSave: true,
       },
       globalSkintone: {
         storage: 'localstorage',
-        allowAutoSave: true
+        allowAutoSave: true,
       },
       individualSkintones: {
         storage: 'localstorage',
-        allowAutoSave: true
-      }
+        allowAutoSave: true,
+      },
     };
   }
 
@@ -362,7 +418,8 @@ export class ChatLayoutComponent implements AfterViewChecked {
   getUser(id: string) {
     const user = this.users.find((u: any) => u._id === id);
     this.user2Data = user!;
-    this.chatService.createOrGetRoom([this.user1Data._id, id])
+    this.chatService
+      .createOrGetRoom([this.user1Data._id, id])
       .subscribe((res: any) => {
         console.log(res);
         this.currentRoomId = res.roomId;
@@ -373,11 +430,11 @@ export class ChatLayoutComponent implements AfterViewChecked {
   }
 
   loadRoomMessages() {
-    this.chatService.getRoomMessages(this.currentRoomId)
-      .subscribe(msgs => this.privateMessages = msgs);
+    this.chatService
+      .getRoomMessages(this.currentRoomId)
+      .subscribe((msgs) => (this.privateMessages = msgs));
 
     this.getAllMessages(this.currentRoomId);
-
   }
 
   sendMessage() {
@@ -392,9 +449,9 @@ export class ChatLayoutComponent implements AfterViewChecked {
     };
     this.socket.emit('stopTyping', {
       to: this.user2Data._id,
-      userId: this.user1Data._id
+      userId: this.user1Data._id,
     });
-    this.showPicker = false
+    this.showPicker = false;
 
     if (file) {
       const reader = new FileReader();
@@ -405,24 +462,31 @@ export class ChatLayoutComponent implements AfterViewChecked {
           filename: file.name,
           mimetype: file.type,
           text,
-          timestamp: new Date()
+          timestamp: new Date(),
         };
         payload.buffer = reader.result as ArrayBuffer;
         this.socket.emit('uploadMessage', payload);
+        this.getAllUsers();
+        this.getAllUnreadMessages();
+        console.log('unreadedcount', this.getAllUnreadMessages());
+
         this.socket.emit('stopTyping', {
           to: this.user2Data._id,
-          userId: this.user1Data._id
+          userId: this.user1Data._id,
         });
       };
       reader.readAsArrayBuffer(file);
-      this.removeSelectedFile()
+      this.removeSelectedFile();
     } else {
       this.socket.emit('sendMessage', payload);
+      this.getAllUsers();
+      this.getAllUnreadMessages();
+      console.log('unreadedcount', this.getAllUnreadMessages());
       this.socket.emit('stopTyping', {
         to: this.user2Data._id,
-        userId: this.user1Data._id
+        userId: this.user1Data._id,
       });
-      this.removeSelectedFile()
+      this.removeSelectedFile();
     }
     this.resetForm();
   }
@@ -439,20 +503,22 @@ export class ChatLayoutComponent implements AfterViewChecked {
       },
       error: (err: any) => {
         console.log(err);
-      }
-    })
+      },
+    });
   }
 
   loadConversations() {
     this.chatService.getAllConversations().subscribe((conversations) => {
-      const privateChats = conversations.filter(c => c.type === 'private');
-      const groupChats = conversations.filter(c => c.type === 'group');
+      const privateChats = conversations.filter((c) => c.type === 'private');
+      const groupChats = conversations.filter((c) => c.type === 'group');
       console.log('Private Chats:', privateChats, groupChats);
 
       this.conversations = [...privateChats, ...groupChats];
 
-      this.conversations = [...privateChats, ...groupChats].sort((a, b) =>
-        new Date(b.lastMessageTime).getTime() - new Date(a.lastMessageTime).getTime()
+      this.conversations = [...privateChats, ...groupChats].sort(
+        (a, b) =>
+          new Date(b.lastMessageTime).getTime() -
+          new Date(a.lastMessageTime).getTime()
       );
 
       console.log('Conversations:', this.conversations);
@@ -466,7 +532,7 @@ export class ChatLayoutComponent implements AfterViewChecked {
       },
       error: (err: any) => {
         console.error('Error fetching starred users', err);
-      }
+      },
     });
   }
 
@@ -487,8 +553,14 @@ export class ChatLayoutComponent implements AfterViewChecked {
       },
       error: (err: any) => {
         console.error('Error fetching messages', err);
-      }
+      },
     });
+  }
+
+  getAllUnreadMessages() {
+    return this.users
+      .map((user: any) => user.unreadCount || 0)
+      .reduce((acc: number, curr: number) => acc + curr, 0);
   }
 
   ngOnDestroy() {
